a62dfdc4fea60a53f416021e96b6fa3d7f2a4dcd
FullstackNumen <academianumen@gmail.com>
2021-05-06 21:17:08 -0300

    first commit

--- 03-JavaScript-I/tareas/tareas.js
+++ 03-JavaScript-I/tareas/tareas.js
@@ -180,7 +180,6 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   //Escribe tu código aquí
-  return "ESTO NO VA"
 }
 
 
